df2a6dbfb65511d5c40d81e3552e39c7f9c264fa
BHARATHARSEN <bcgrkofficial@gmail.com>
2024-04-01 13:04:10 -0500

    Added new functions (comapre password and resetpassword token

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,5 +1,7 @@
-const mongoose = require("mongoose");
-
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import crypto from "crypto";
 const userSchema = new mongoose.Schema(
   {
     name: {
@@ -32,5 +34,44 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+// Encrypting password before saving the user
+userSchema.pre('save', async function (next) {
+  if(!this.isModified("password")) {
+    next();
+  }
+
+  this.password = await bcrypt.hash(this.password, 10)
+});
+
+//Return JWT token
+
+userSchema.methods.getJwtToken = function () {
+  return jwt.sign({ id : this._id}, process.env.JWT_SECRET, {
+    expiresIn : process.env.JWT_EXPIRES_TIME
+  })
+
+}
+
+userSchema.methods.comparePassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password)
+
+}
+
+userSchema.methods.getResetPasswordToken = function () {
+
+  const resetToken = crypto.randomBytes(20).toString('hex');
+
+  // Hash and set the resetPasswordToken field
+
+  this.resetPasswordToken = crypto.createHash("sha256").update(resetToken).digest('hex');
+  // set expire time for the token 
+
+  this.resetPasswordExpire = Date.now() + 30 *60 *1000
+
+  return resetToken;
+};
+
+
 
 export default mongoose.model("User", userSchema);
