7f72cda28cb6c1a5b06cffbe9a6f6cfdc5ab3fd8
BHARATHARSEN <bcgrkofficial@gmail.com>
2024-04-03 20:07:50 -0500

    Added Order Model

---
+++ backend/models/order.js
@@ -0,0 +1,114 @@
+import mongoose from "mongoose";
+
+const orderSchema = new mongoose.Schema(
+  {
+    shippingInfo: {
+        address: {
+          type: String,
+          required: true,
+        },
+        city: {
+          type: String,
+          required: true,
+        },
+        phoneNo: {
+          type: String,
+          required: true,
+        },
+        zipCode: {
+          type: String,
+          required: true,
+        },
+        country: {
+          type: String,
+          required: true,
+        },
+      },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User", // Reference to the User model
+      required: true,
+    },
+    orderItems: [
+      {
+        name: {
+          type: String,
+          required: true,
+        },
+        quantity: {
+          type: Number,
+          required: true,
+        },
+        image: {
+          type: String,
+          required: true,
+        },
+        price: {
+          type: Number,
+          required: true,
+        },
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product", // Reference to the Product model
+          required: true,
+        },
+        // You can add additional fields for item details like name, image, etc.
+      },
+    ],
+    paymentMethod: {
+      type: String,
+      required: [true, "Please select the payment method"],
+      enum: {
+        values: ["COD", "Card"],
+        message: "Please select : COD or Card",
+      },
+    },
+    paymentInfo: {
+      id: String,
+      status: String,
+    },
+    itemsPrice: {
+      type: Number,
+      required: true,
+    },
+    shippingAddress: {
+      type: Object,
+      required: true,
+      properties: {
+        fullName: {
+          type: String,
+          required: true,
+        },
+        addressLine1: {
+          type: String,
+          required: true,
+        },
+        // Add other address details like city, state, zip code, country
+      },
+    },
+    taxAmount: {
+      type: Number,
+      required: true,
+    },
+    shippingAmount: {
+      type: Number,
+      required: true,
+    },
+    totalAmount: {
+      type: Number,
+      required: true,
+    },
+    orderStatus: {
+      type: String,
+      enum: {
+        values: ["Processing", "Shipped", "Delivered"],
+        message: "Please select correct order status",
+      },
+      default: "Processing",
+    },
+    deliveredAt: Date,
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Order", orderSchema);
