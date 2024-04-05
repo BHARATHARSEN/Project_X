31436a89ee5c89d686b59926d85ec190bbb24d71
BHARATHARSEN <bcgrkofficial@gmail.com>
2024-04-05 16:46:21 -0500

    Added new routes for review functionality

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,11 +1,36 @@
 import express from 'express';
 import { deleteProduct, getProductDetails, getProducts, newProduct, updateProduct } from "../controllers/productControllers.js";
+import { isAuthenticatedUser,authorizeRoles } from '../middlewares/auth.js';
+import { createProductReview, deleteReview, getProductReviews } from '../controllers/orderControllers.js';
 const router = express.Router();
 
 router.route("/products").get(getProducts);
+router
+  .route("/admin/products")
+  .post(isAuthenticatedUser, authorizeRoles("admin"), newProduct);
+
+router.route("/admin/products")
+  .get(isAuthenticatedUser, authorizeRoles("admin"), getProducts);
+
+  
 router.route("/products/:id").get(getProductDetails);
-router.route("/admin/products").post(newProduct);
-router.route("/products/:id").put(updateProduct);
-router.route("/products/:id").delete(deleteProduct);
+
+
+router
+  .route("/admin/products/:id")
+  .put(isAuthenticatedUser, authorizeRoles("admin"), updateProduct);
+router
+  .route("/admin/products/:id")
+  .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteProduct);
+
+router
+  .route("/reviews")
+  .get(isAuthenticatedUser,getProductReviews)
+  .put(isAuthenticatedUser,createProductReview );
+
+router
+  .route("/admin/reviews")
+  .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteReview);
+
 
 export default router ;
