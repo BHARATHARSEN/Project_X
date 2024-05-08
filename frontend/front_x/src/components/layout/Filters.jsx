7fdedfc4e5736ffad4c90c13ce6b3d9bfcabfa6d
BHARATHARSEN <bcgrkofficial@gmail.com>
2024-05-08 16:09:47 -0500

    Added Filters for categories and min & max using searchParams and checkboxes using (getPriceQueryparams)

--- frontend/front_x/src/components/layout/Filters.jsx
+++ frontend/front_x/src/components/layout/Filters.jsx
@@ -14,6 +14,7 @@ const Filters = () => {
 
     useEffect(() => {
         searchParams.has("min") && setMin(searchParams.get("min"));
+        
         searchParams.has("max") && setMax(searchParams.get("max"));
 
     },[])
