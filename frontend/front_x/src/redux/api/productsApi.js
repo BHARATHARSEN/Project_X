2bfcd02e00699765d8af299317c03a506d58125f
BHARATHARSEN <bcgrkofficial@gmail.com>
2024-05-08 16:15:47 -0500

    ProductsApi to build using params : page,keyword,category,price[gte],price[lte],ratings[gte], and getProductsQuery

--- frontend/front_x/src/redux/api/productsApi.js
+++ frontend/front_x/src/redux/api/productsApi.js
@@ -1,14 +1,29 @@
-import {createApi, fetchBaseQuery} from '@reduxjs/toolkit';
+import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 
 export const productApi = createApi({
-    reducerPath : "productApi", // slice name
-    baseQuery : fetchBaseQuery({baseUrl : "api/v1"}),
-    endpoints : (builder) => ({
-        getProducts: builder.query({
-            query : (params) => ("/products")
-        }),
-
+  reducerPath: "productApi", // slice name
+  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3000/api/v1" }),
+  keepUnusedDataFor: 30,
+  endpoints: (builder) => ({
+    getProducts: builder.query({
+      query: (params) => ({
+        url: "/products",
+        params: {
+          page: params?.page,
+          keyword: params?.keyword,
+          category: params?.category,
+          "price[gte]": params.min,
+          "price[lte]": params.max,
+          "ratings[gte]": params?.ratings
+          
+        },
+      }),
     }),
+    getProductDetails: builder.query({
+      query: (id) => `/product/${id}`,
+    }),
+  }),
 });
 
-export const { useGetProductsQuery } = productApi
+
+export const { useGetProductsQuery, useGetProductDetailsQuery } = productApi ;
