725b9cbc19cf2b538abf5ac335ecea819c33b4b6
BHARATHARSEN <bcgrkofficial@gmail.com>
2024-05-09 18:09:28 -0500

    added routes in App.js till Update Profile

---
+++ frontend/front_x/src/App.js
@@ -0,0 +1,37 @@
+import Footer from "./components/layout/Footer";
+import Header from "./components/layout/Header";
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import "./App.css";
+import Home from "./components/Home";
+import {Toaster} from 'react-hot-toast'
+import ProductDetails from "./components/product/ProductDetails";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
+import Profile from "./components/user/Profile";
+import UpdateUser from "./components/user/UpdateUser";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Toaster position="top-center" />
+        <Header />
+
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/product/:id" element={<ProductDetails />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/me/Profile" element={<Profile />} />
+            <Route path="/me/update" element={<UpdateUser />} />
+          </Routes>
+        </div>
+
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
