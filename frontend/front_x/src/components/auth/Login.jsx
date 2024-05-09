0cce3e6309f83456c2828707944ed77782a4a499
BHARATHARSEN <bcgrkofficial@gmail.com>
2024-05-09 17:06:59 -0500

    Updated Login with Name and Password fields

--- frontend/front_x/src/components/auth/Login.jsx
+++ frontend/front_x/src/components/auth/Login.jsx
@@ -1,57 +1,52 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { useLoginMutation } from '../../redux/api/authApi';
 import toast from 'react-hot-toast';
+import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 const Login = () => {
 
-    const[email, setEmail] = useState("")
-    const[password,setPassword] = useState("")
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const [login, {isLoading, error, data}] = useLoginMutation();
+  const [login, { isLoading, error, data }] = useLoginMutation();
+  const { isAuthenticated } = useSelector((state) => state.auth);
 
-    console.log("+++++++++++++++++++++++++++++++++++")
+  console.log("+++++++++++++++++++++++++++++++++++");
+  console.log(data);
+  console.log("+++++++++++++++++++++++++++++++++++");
 
-    console.log(data)
+  useEffect(() => {
+    if (isAuthenticated) {
+      navigate("/");
+    }
 
+    if (error) {
+      toast.error(error?.data?.message);
+    }
+  }, [error, isAuthenticated]);
 
+  const submitHandler = (e) => {
+    e.preventDefault();
 
-    console.log("+++++++++++++++++++++++++++++++++++")
-
-    useEffect(() => {
-        if(error){
-            toast.error(error?.data?.message);
-
-        }
-    },[error])
-
-
-    const submitHandler = (e) => {
-        e.preventDefault();
-
-        const loginData = {
-            email,
-            password,
-
-            
-        };
-
-        login(loginData);
+    const loginData = {
+      email,
+      password,
     };
 
-
-
-
+    login(loginData);
+  };
 
   return (
-
     <div className="row wrapper">
       <div className="col-10 col-lg-5">
-        <form
-          className="shadow rounded bg-body"onSubmit={submitHandler}
-        >
+        <form className="shadow rounded bg-body" onSubmit={submitHandler}>
           <h2 className="mb-4">Login</h2>
           <div className="mb-3">
-            <label htmlFor="email_field" className="form-label">Email</label>
+            <label htmlFor="email_field" className="form-label">
+              Email
+            </label>
             <input
               type="email"
               id="email_field"
@@ -63,7 +58,9 @@ const Login = () => {
           </div>
 
           <div className="mb-3">
-            <label htmlFor="password_field" className="form-label">Password</label>
+            <label htmlFor="password_field" className="form-label">
+              Password
+            </label>
             <input
               type="password"
               id="password_field"
@@ -74,19 +71,28 @@ const Login = () => {
             />
           </div>
 
-          <a href="/password/forgot" className="float-end mb-4">Forgot Password?</a>
+          <a href="/password/forgot" className="float-end mb-4">
+            Forgot Password?
+          </a>
 
-          <button id="login_button" type="submit" className="btn w-100 py-2" disabled = {isLoading}>
-            {isLoading? "Authenticating..." : "LOGIN"}
+          <button
+            id="login_button"
+            type="submit"
+            className="btn w-100 py-2"
+            disabled={isLoading}
+          >
+            {isLoading ? "Authenticating..." : "LOGIN"}
           </button>
 
           <div className="my-3">
-            <a href="/register" className="float-end">New User?</a>
+            <a href="/register" className="float-end">
+              New User?
+            </a>
           </div>
         </form>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
