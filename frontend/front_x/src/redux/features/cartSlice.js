fada4aaeccaca20cc09ba00a802069635199586d
BHARATHARSEN <bcgrkofficial@gmail.com>
2024-05-15 16:40:05 -0500

    Added new reducer named removeCartItem

--- frontend/front_x/src/redux/features/cartSlice.js
+++ frontend/front_x/src/redux/features/cartSlice.js
@@ -1,7 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  cartItems : localStorage.getItem("cartItems") ? JSON.parse(("cartItems")) : [],
+  cartItems : localStorage.getItem("cartItems") ? JSON.parse(localStorage.getItem("cartItems")) : [],
 };
 
 export const cartSlice = createSlice({
@@ -9,23 +9,35 @@ export const cartSlice = createSlice({
   name: "cartSlice",
 
   reducers: {
-    setCartItem : (state,action)=> {
-        const item = action.payload;
+    setCartItem: (state, action) => {
+      const item = action.payload;
 
-        const isItemExist = state.cartItems.find(
-            (i) => i.product === item.product
+      const isItemExist = state.cartItems.find(
+        (i) => i.product === item.product
+      );
+
+      if (isItemExist) {
+        state.cartItems = state.cartItems.map((i) =>
+          i.product === isItemExist.product ? item : i
         );
+      } else {
+        state.cartItems = [...state.cartItems, item];
+      }
 
-        if(isItemExist){
-            state.cartItems = state.cartItems.map((i) => i.product === isItemExist.product ? item : i );
-        } else {
-            state.cartItems = [...state.cartItems, item];
-        }
+      localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
+    },
 
-        localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
-    }
+    removeCartItem: (state, action) => {
+      const productId = action.payload;
+
+      state.cartItems = state?.cartItems?.filter(
+        (item) => item.product !== productId
+      );
+
+      localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
+    },
   },
 });
 
 export default cartSlice.reducer;
-export const { setCartItem} = cartSlice.actions;
+export const { setCartItem, removeCartItem} = cartSlice.actions;
