5cbd5e08b1cf097ac09b77bb9cb6076ee94c046e
BHARATHARSEN <bcgrkofficial@gmail.com>
2024-05-15 16:29:39 -0500

    Added Meta data and empty tags

--- frontend/front_x/src/components/auth/ForgotPassword.jsx
+++ frontend/front_x/src/components/auth/ForgotPassword.jsx
@@ -3,6 +3,7 @@ import { useForgotPasswordMutation } from '../../redux/api/userApi';
 import { useNavigate } from 'react-router-dom';
 import toast from 'react-hot-toast';
 import { useSelector } from 'react-redux';
+import MetaData from '../layout/MetaData';
 
 
 const ForgotPassword = () => {
@@ -37,37 +38,41 @@ const ForgotPassword = () => {
 
 
   return (
+    <>
+    <MetaData title={"Forgot Password"}/>
     
-      <div className="row wrapper">
-      <div className="col-10 col-lg-5">
-        <form
-          className="shadow rounded bg-body"
-          onSubmit={submitHandler}
-        >
-          <h2 className="mb-4">Forgot Password</h2>
-          <div className="mt-3">
-            <label htmlfor="email_field" className="form-label">Enter Email</label>
-            <input
-              type="email"
-              id="email_field"
-              className="form-control"
-              name="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </div>
+    <div className="row wrapper">
+    <div className="col-10 col-lg-5">
+      <form
+        className="shadow rounded bg-body"
+        onSubmit={submitHandler}
+      >
+        <h2 className="mb-4">Forgot Password</h2>
+        <div className="mt-3">
+          <label htmlfor="email_field" className="form-label">Enter Email</label>
+          <input
+            type="email"
+            id="email_field"
+            className="form-control"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
 
-          <button
-            id="forgot_password_button"
-            type="submit"
-            className="btn w-100 py-2"
-            disabled = {isLoading}
-          >
-            {isLoading ? "Sending email" : "Send Email"}
-          </button>
-        </form>
-      </div>
+        <button
+          id="forgot_password_button"
+          type="submit"
+          className="btn w-100 py-2"
+          disabled = {isLoading}
+        >
+          {isLoading ? "Sending email" : "Send Email"}
+        </button>
+      </form>
     </div>
+  </div>
+    </>
+       
     
   )
 }
