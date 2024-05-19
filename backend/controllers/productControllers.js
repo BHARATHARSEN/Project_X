d339dd99776eab863e089a43a50c0a561ce045b6
BHARATHARSEN <bcgrkofficial@gmail.com>
2024-05-19 04:19:07 -0500

    Changed canUserReview function

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -5,6 +5,7 @@ import catchAsyncErrors from "../middlewares/catchAsyncErrors.js";
 import Product from "../models/product.js";
 import ErrorHandler from "../utils/errorHandler.js";
 import APIFilters from "../utils/apiFilters.js";
+import Order from "../models/order.js";
 
 export const getProducts = catchAsyncErrors(async (req,res,next) => {
     const resPerPage = 4;
@@ -81,6 +82,134 @@ export const deleteProduct = catchAsyncErrors( async (req, res) => {
   await product.deleteOne();
 
   res.status(200).json({ message : "Product Deleted",});
+});
+
+
+
+//=======================Review Section functionalities=======================
+
+// review : [
+//   {
+//     user : {},
+//     rating : {},
+//     comment:{},
+//   }
+// ]
+
+export const createProductReview = catchAsyncErrors(async (req, res, next) => {
+
+  const {rating, comment, productId} = req.body;
+
+  const review = {
+    user: req?.user?._id,
+    rating: Number(rating),
+    comment,
+  }
+
+  const product = await Product.findById(productId);
+
+  if(!product){
+      return next(new ErrorHandler('Product is not found', 404));
+  }
+
+  const isReviewed = product?.reviews?.find(
+    (r) => r.user.toString() === req?.user?._id.toString()
+  );
+
+  if(isReviewed){
+    product.reviews.forEach((review) => {
+      if(review.user.toString() === req?.user?._id.toString()){
+        review.comment = comment;
+        review.rating = rating ;
+
+      }
+
+    });
+
+  }else{
+    product.reviews.push(review);
+    product.numOfReviews = product.reviews.length;
+
+  }
+
+
+  product.ratings = product.reviews.reduce((acc,item) => item.rating + acc, 0)
+   /product.reviews.length;
+
+  await product.save({validateBeforeSave : false})
+
+  res.status(200).json({
+    success: true,
+  });
+});
+
+
+//================================================================================================
+
+export const getProductReviews = catchAsyncErrors( async (req, res, next) => {
+  const product = await Product.findById(req.query.id)
+
+  if (!product) {
+    return next(new ErrorHandler("Product is not found", 404));
+  }
+
+  res.status(200).json({
+    reviews: product.reviews,
+  });
+});
+
+//=================================================================================================
+
+export const deleteReview = catchAsyncErrors(async (req, res, next) => {
+  // const { rating, comment, productId } = req.body;
+
+  // const review = {
+  //   user: req?.user?._id,
+  //   rating: Number(rating),
+  //   comment,
+  // };
+
+  let product = await Product.findById(req.query.productId);
+
+  if (!product) {
+    return next(new ErrorHandler("Product is not found", 404));
+  }
+
+  const reviews = product?.reviews?.filter(
+    (review) => review._id.toString() !== req?.query?.id.toString()
+  );
+
+  const numOfReviews = reviews.length;
+
+  const ratings =
+    numOfReviews === 0 
+      ? 0
+      :product.reviews.reduce((acc, item) => item.rating + acc, 0) /
+      product.reviews.length;
+
+  product = await Product.findByIdAndUpdate(req.query.productId, {reviews,numOfReviews,ratings}, {new:true})
+
+  res.status(200).json({
+    success: true,
+    product
+  });
+});
+
+
+// Can user give a review  => /api/v1/can_review
+
+export const canUserReview = catchAsyncErrors(async (req, res) => {
+  
+  const orders = await Order.find({
+    user : req.user._id,
+    "orderItems.product": req.product.productId,
+  });
+
+  if(orders.length === 0){
+    res.status(200).json({canReviewed :false});
+  }
+
+  res.status(200).json({ canReviewed: true });
 });
 
 
