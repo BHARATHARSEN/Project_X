25711cc101dbf11448f7107b13d744077c902f8c
BHARATHARSEN <bcgrkofficial@gmail.com>
2024-05-22 04:57:38 -0500

    Added sales Data for orders to show in chart

--- backend/controllers/orderControllers.js
+++ backend/controllers/orderControllers.js
@@ -128,6 +128,115 @@ export const deleteOrder = catchAsyncErrors(async (req, res, next) => {
   });
 });
 
+//                             GETTING SALES DATA OF ALL ORDERS FOR CHART 
+
+//=============================================================================
+async function getSalesData(startDate, endDate) {
+  console.log("Fetching sales data between", startDate, "and", endDate);
+  const salesData = await Order.aggregate([
+    {
+      $match: {
+        createdAt: {
+          $gte: new Date(startDate),
+          $lte: new Date(endDate),
+        },
+      },
+    },
+    {
+      $group: {
+        _id: {
+          date: { $dateToString: { format: "%Y-%m-%d", date: "$createdAt" } },
+        },
+        totalSales: { $sum: "$totalAmount" },
+        numOrder: { $sum: 1 },
+      },
+    },
+  ]);
+
+  console.log("Sales data retrieved:", salesData);
+
+  const salesMap = new Map();
+
+  let totalSales = 0;
+  let totalNumOrders = 0;
+
+  salesData.forEach((entry) => {
+    const date = entry?._id.date;
+    const sales = entry?.totalSales;
+    const numOrders = entry?.numOrder; 
+
+    salesMap.set(date, { sales, numOrders });
+    totalSales += sales;
+    totalNumOrders += numOrders;
+  });
+
+  const datesBetween = getDatesBetween(startDate, endDate);
+
+  console.log("Processed sales data map:", salesMap);
+  console.log("Total sales:", totalSales);
+  console.log("Total number of orders:", totalNumOrders);
+  console.log("Dates between:", datesBetween);
+
+  // Creating final sales data array with o for dates with no sales :(
+
+  const finalSalesData = datesBetween.map((date) => ({
+    date,
+    sales: (salesMap.get(date) || { sales: 0 }).sales,
+    numOrders: (salesMap.get(date) || { numOrders: 0 }).numOrders,
+  }));
+
+  console.log("===============================")
+
+  console.log(finalSalesData);
+
+  console.log("===============================");
+
+  return {salesData : finalSalesData, totalSales, totalNumOrders};
+
+  
+}
+
+
+
+
+function getDatesBetween(startDate, endDate) {
+  const dates = [];
+  let currentDate = new Date(startDate);
+
+  while (currentDate <= new Date(endDate)) {
+    const formattedDate = currentDate.toISOString().split("T")[0];
+    dates.push(formattedDate);
+    currentDate.setDate(currentDate.getDate() + 1);
+  }
+
+  return dates;
+}
+
+// Get Sales Data =>  /api/v1/admin/get_sales
+export const getSales = catchAsyncErrors(async (req, res, next) => {
+  try {
+    const startDate = new Date(req.query.startDate);
+    const endDate = new Date(req.query.endDate);
+
+    startDate.setUTCHours(0, 0, 0, 0);
+    endDate.setUTCHours(23, 59, 59, 999);
+
+    const {salesData, totalSales, totalNumOrders} = await getSalesData(startDate, endDate);
+
+    res.status(200).json({
+      totalSales,
+      totalNumOrders,
+      sales : salesData,
+
+      
+    });
+  } catch (error) {
+    next(error); // Proper error handling
+  }
+});
+
+
+
 
 
 
