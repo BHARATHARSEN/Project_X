41bfc2668102cb3eddc9fca2f4e2badc9f9073d1
BHARATHARSEN <bcgrkofficial@gmail.com>
2024-05-24 05:00:32 -0500

    Imported getDashboardSales method for data

--- frontend/front_x/src/components/admin/Dashboard.jsx
+++ frontend/front_x/src/components/admin/Dashboard.jsx
@@ -1,27 +1,53 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import AdminLayout from "../layout/AdminLayout";
 import DatePicker from "react-datepicker";
-import 'react-datepicker/dist/react-datepicker.css';
-import SalesChart from "../charts/SalesChart.jsx"
-
+import "react-datepicker/dist/react-datepicker.css";
+import SalesChart from "../charts/SalesChart.jsx";
+import toast from "react-hot-toast";
+import { useLazyGetDashboardSalesQuery } from "../../redux/api/orderApi.js";
+import Loader from "../layout/Loader.jsx";
+import MetaData from "../layout/MetaData.jsx";
 
 const Dashboard = () => {
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
 
+  const [getDashboardSales, { error, isLoading, data }] =
+    useLazyGetDashboardSalesQuery();
+
+  useEffect(() => {
+    if (error) {
+      toast.error(error?.data?.message);
+    }
+
+    if (startDate && endDate && !data) {
+      getDashboardSales({
+        startDate: new Date(startDate).toISOString(),
+        endDate: endDate.toISOString(),
+      });
+    }
+  }, [error]);
+
   const submitHandler = () => {
-    console.log("==================")
+    // console.log("==================")
 
-    console.log(startDate.toISOString());
+    // console.log(startDate.toISOString());
 
-    console.log(endDate.toISOString());
+    // console.log(endDate.toISOString());
 
+    // console.log("==================");
 
-    console.log("==================")
-  }
+    getDashboardSales({
+      startDate: new Date(startDate).toISOString(),
+      endDate: endDate.toISOString(),
+    });
+  };
+
+  if (isLoading) return <Loader />;
 
   return (
     <AdminLayout>
+      <MetaData title={"Dashboard"} />
       <div className="d-flex justify-content-start align-items-center">
         <div className="mb-3 me-4">
           <label className="form-label d-block">Start Date</label>
@@ -47,7 +73,12 @@ const Dashboard = () => {
             className="form-control"
           />
         </div>
-        <button className="btn fetch-btn ms-4 mt-3 px-5" onClick={submitHandler}>Fetch</button>
+        <button
+          className="btn fetch-btn ms-4 mt-3 px-5"
+          onClick={submitHandler}
+        >
+          Fetch
+        </button>
       </div>
 
       <div className="row pr-4 my-5">
@@ -57,7 +88,7 @@ const Dashboard = () => {
               <div className="text-center card-font-size">
                 Sales
                 <br />
-                <b>$0.00</b>
+                <b>${data?.totalSales.toFixed(2)}</b>
               </div>
             </div>
           </div>
@@ -69,14 +100,14 @@ const Dashboard = () => {
               <div className="text-center card-font-size">
                 Orders
                 <br />
-                <b>0</b>
+                <b>{data?.totalNumOrders}</b>
               </div>
             </div>
           </div>
         </div>
       </div>
 
-      <SalesChart />
+      <SalesChart salesData={data?.sales} />
 
       <div className="mb-5"></div>
     </AdminLayout>
