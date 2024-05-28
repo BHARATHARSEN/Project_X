4cf7a4f43c933ccf5b03053fb70fe8d215b5acba
BHARATHARSEN <bcgrkofficial@gmail.com>
2024-05-28 16:44:35 -0500

    Customized getProductReviews by populating it with name and reviews

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -217,7 +217,10 @@ export const createProductReview = catchAsyncErrors(async (req, res, next) => {
 //================================================================================================
 
 export const getProductReviews = catchAsyncErrors( async (req, res, next) => {
-  const product = await Product.findById(req.query.id)
+  const product = await Product.findById(req.query.id).populate(
+    "reviews.user",
+    "name"
+  );
 
   if (!product) {
     return next(new ErrorHandler("Product is not found", 404));
